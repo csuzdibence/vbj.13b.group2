d07443c226261cb75c67929bc66c4defae23d7fb
Bence Csuzdi <csuzdibence@gmail.com>
2024-09-23 11:22:00 +0200

    Package class has been implemented

---
+++ VBJ.13B.Group2/VBJ.13B.Group2.Console/Package.cs
@@ -0,0 +1,23 @@
+﻿namespace VBJ._13B.Group2
+{
+    /// <summary>
+    /// Csomag osztály, ezt viheti ki a Courier
+    /// </summary>
+    public class Package
+    {
+        // Field/mező -> olyan mint egy lokális változó
+        // private -> csak osztályon belül látható
+        // public ->akkor osztályon kívül is láthatü
+        // Fontos: mezők mindig private-k
+        private static int id = 1;
+
+        // Konstruktor -> shortcut ctor után tab
+        public Package()
+        {
+            ID = id++;
+        }
+
+        // Tulajdonság/property hozzáférhetőség a mezőhöz
+        public int ID { get; }
+    }
+}
--- VBJ.13B.Group2/VBJ.13B.Group2.Console/Program.cs
+++ VBJ.13B.Group2/VBJ.13B.Group2.Console/Program.cs
@@ -1,3 +1,19 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello 13.B!");
+﻿using System;
+using VBJ._13B.Group2;
+
+public class Program 
+{
+    // Konzol alkalmazás belépési pontja -> rányomsz az exére
+    public static void Main()
+    {
+        Package package = new Package();
+        Package package2 = new Package();
+        Package package3 = new Package();
+        // Futár felvétele
+
+        // Csomaghozzárendelés
+
+        // Csomagszállítás
+        Console.WriteLine();
+    }
+}
