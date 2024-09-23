4c938574cebcb2d3b86393cda08858cbaff48e9c
Bence Csuzdi <csuzdibence@gmail.com>
2024-09-23 13:31:36 +0200

    OOP gyakorló feladat added

---
+++ VBJ.13B.Group2/VBJ.13B.Group2.OOPGyakorlo1/Prison.cs
@@ -0,0 +1,55 @@
+﻿namespace VBJ._13B.Group2.OOPGyakorlo1
+{
+    public class Prison
+    {
+        private static int idIndex = 0;
+        Prisoner[] prisoners;
+        public Prison(int size)
+        {
+            prisoners = new Prisoner[size];
+        }
+
+        public bool AddPrisoner(Prisoner prisoner)
+        {
+            for (int i = 0; i < prisoners.Length; i++)
+            {
+                if (prisoners[i] is null)
+                {
+                    prisoners[i] = prisoner;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string PrisonerList()
+        {
+            string text = string.Empty;
+            foreach (var prisoner in prisoners)
+            {
+                text += (prisoner.PrisonerText + "\n");
+            }
+            return text;
+        }
+
+        public void Escape()
+        {
+            for (int i = 0; i < prisoners.Length; i++)
+            {
+                if (prisoners[i] is null)
+                {
+                    continue;
+                }
+
+                if (prisoners[i].Smartness > 50)
+                {
+                    prisoners[i] = null;
+                }
+                else
+                {
+                    prisoners[i].Smartness += 5;
+                }
+            }
+        }
+    }
+}
---
+++ VBJ.13B.Group2/VBJ.13B.Group2.OOPGyakorlo1/Prisoner.cs
@@ -0,0 +1,19 @@
+﻿namespace VBJ._13B.Group2.OOPGyakorlo1
+{
+    public class Prisoner
+    {
+        public Prisoner(string name, int id)
+        {
+            Name = name;
+            ID = id;
+            Smartness = new Random().Next(1, 101);
+        }
+
+        public string Name { get; }
+        public int ID { get; }
+
+        public int Smartness { get; set; }
+
+        public string PrisonerText => $"{Name} #{ID}";
+    }
+}
