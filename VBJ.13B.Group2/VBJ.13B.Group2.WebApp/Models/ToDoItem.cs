4ce4cbe9586855351c0467bc0256db7f8dcc6bc0
Bence Csuzdi <csuzdibence@gmail.com>
2024-10-07 13:22:54 +0200

    ToDoItem has been created

---
+++ VBJ.13B.Group2/VBJ.13B.Group2.WebApp/Models/ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿namespace VBJ._13B.Group2.WebApp.Models
+{
+    /// <summary>
+    /// Ez az osztály jelképez egy elvégzendő teendőt
+    /// </summary>
+    public class ToDoItem
+    {
+        /// <summary>
+        /// Az azonosítója
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Az elvégzendő teendő címe
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Azt jelöli hogy a teendő elvégzésre került vagy sem
+        /// </summary>
+        public bool HasCompleted { get; set; }
+    }
+}
