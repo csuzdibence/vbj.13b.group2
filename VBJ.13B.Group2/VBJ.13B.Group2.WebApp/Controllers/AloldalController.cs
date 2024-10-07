c39171ecc3a953005a6fb7d0fa4167b2385fccf4
Bence Csuzdi <csuzdibence@gmail.com>
2024-10-07 12:01:25 +0200

    Hello world exercise has been completed

---
+++ VBJ.13B.Group2/VBJ.13B.Group2.WebApp/Controllers/AloldalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VBJ._13B.Group2.WebApp.Controllers
+{
+    public class AloldalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VBJ.13B.Group2/VBJ.13B.Group2.WebApp/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VBJ._13B.Group2.WebApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // /Hello -> ami a feladatban volt
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello world";
+            return View();
+        }
+    }
+}
